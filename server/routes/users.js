c3cc96dab2adcebdec3e78d1842c4f7ad83c4e98
David Harris <bblldave@gmail.com>
2023-06-07 21:14:10 -0400

    Created the login api endpoint

--- server/routes/users.js
+++ server/routes/users.js
@@ -23,4 +23,30 @@ router.post('/register', async (req, res) => {
     }
 });
 
+router.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Find the user by username
+        const user = await User.findOne({ username });
+
+        // If the user wasnt found or the password isnt right, send an error
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(400).json({ message: 'Username or Password is incorrect'});
+        }
+
+        // If all is good, create json web token
+        const token = jwt.sign({ userId: user._id}, process.env.JWT_SECRET);
+
+        // Send the token and user in the response
+        const userObj = user.toObject();
+        delete userObj.password;
+        res.json({ token, user: userObj });
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Something went wrong!' });
+    }
+})
+
 module.exports = router;
