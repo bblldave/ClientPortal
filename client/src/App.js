501ed1efd38e2f727d325a3f09e113225bd9ec5a
David Harris <bblldave@gmail.com>
2023-06-07 21:15:03 -0400

    setup mui and styled register component

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import React from 'react'
-import ReactDOM from 'react-dom/client';
 
 function App() {
   return (
